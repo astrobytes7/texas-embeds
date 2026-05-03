18e1007365047aa08ec86a329762c1ec6ee9b746
Eshan <astrobytesgaming@gmail.com>
2026-05-03 15:13:08 -0700

    Auto-commit: changes detected

---
+++ config.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+module.exports = {
+    TOKEN: process.env.TOKEN,
+    APP_ID: "1500532542116466960",
+    mongoURL: "", 
+    GUILD_ID: "1485737998611120161",
+    prefix: "a!"
+};
